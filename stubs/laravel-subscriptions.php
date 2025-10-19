4ad756729741225077b0727a6abeb82e3f0d6f9e
Andrej Prus <goleaf@gmail.com>
2025-10-19 23:24:33 +0200

    Add Laravel subscriptions stub to PHPStan (#202)

---
+++ stubs/laravel-subscriptions.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Laravelcm\Subscriptions\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * @property int $id
+ */
+class Plan extends Model
+{
+}
+
+/**
+ * @property int $id
+ */
+class Feature extends Model
+{
+}
+
+/**
+ * @property int $id
+ */
+class Subscription extends Model
+{
+}
+
+/**
+ * @property int $id
+ */
+class SubscriptionUsage extends Model
+{
+}
