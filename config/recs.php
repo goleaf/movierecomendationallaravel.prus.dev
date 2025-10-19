5ab6f06cae6a6596b6c27b8b81563394026eb887
Andrej Prus <goleaf@gmail.com>
2025-10-19 20:20:50 +0200

    Use env-configurable recommendation weights

--- config/recs.php
+++ config/recs.php
@@ -2,13 +2,13 @@
 
 return [
     'A' => [
-        'pop' => (float) env('REC_A_POP', 0.7),
-        'recent' => (float) env('REC_A_RECENT', 0.2),
-        'pref' => (float) env('REC_A_PREF', 0.1),
+        'pop' => (float) env('REC_A_POP', 0.55),
+        'recent' => (float) env('REC_A_RECENT', 0.20),
+        'pref' => (float) env('REC_A_PREF', 0.25),
     ],
     'B' => [
-        'pop' => (float) env('REC_B_POP', 0.3),
-        'recent' => (float) env('REC_B_RECENT', 0.6),
-        'pref' => (float) env('REC_B_PREF', 0.1),
+        'pop' => (float) env('REC_B_POP', 0.35),
+        'recent' => (float) env('REC_B_RECENT', 0.15),
+        'pref' => (float) env('REC_B_PREF', 0.50),
     ],
 ];
