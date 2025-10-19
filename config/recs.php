4957e27f02c10e445f475904a90eace294fab228
Andrej Prus <goleaf@gmail.com>
2025-10-19 20:14:27 +0200

    Read recommendation weights from environment (#60)

--- config/recs.php
+++ config/recs.php
@@ -2,13 +2,13 @@
 
 return [
     'A' => [
-        'pop' => (float) env('REC_A_POP', 0.7),
-        'recent' => (float) env('REC_A_RECENT', 0.2),
-        'pref' => (float) env('REC_A_PREF', 0.1),
+        'pop' => floatval(env('REC_A_POP', 0.7)),
+        'recent' => floatval(env('REC_A_RECENT', 0.2)),
+        'pref' => floatval(env('REC_A_PREF', 0.1)),
     ],
     'B' => [
-        'pop' => (float) env('REC_B_POP', 0.3),
-        'recent' => (float) env('REC_B_RECENT', 0.6),
-        'pref' => (float) env('REC_B_PREF', 0.1),
+        'pop' => floatval(env('REC_B_POP', 0.3)),
+        'recent' => floatval(env('REC_B_RECENT', 0.6)),
+        'pref' => floatval(env('REC_B_PREF', 0.1)),
     ],
 ];
